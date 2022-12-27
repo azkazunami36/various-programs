5a8f1152a6ea5c0bc0be96cfcd13517d9c1a793f
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-27 17:35:33 +0900

    かるーく修正

--- bot/main.js
+++ bot/main.js
@@ -1,5 +1,5 @@
 const { count } = require('console');
-const { Client, GatewayIntentBits, Message,EmbedBuilder,MessageActionRow, MessageButton } = require('discord.js');
+const { Client, GatewayIntentBits, Message, EmbedBuilder, MessageActionRow, MessageButton } = require('discord.js');
 require("dotenv").config();
 require('date-utils');
 let cmdexec = 0;
@@ -53,34 +53,32 @@ const loga = 0
 client.on('messageCreate', message => {  //切れてるのか横も
     if (message.author.bot) return;
 
-    for (let i = 0; i != data.data.length; i++) {
-    for (let i = 0; i != data.ok.length; i++){
-        if(message.author.id == data.ok)
-        return 
-        if (message.content.match(data.data[i])) {
-            counta++
-            console.log(data.data[i] + "とマッチしました")
+    for (let i = 0; i != data.data.length; i++) { //dataの数だけ
+        for (let i = 0; i != data.ok.length; i++) {
+            if (message.author.id == data.ok[i]) return //returnの後に実行したいコードを入力すること
+            if (message.content.match(data.data[i])) {
+                counta++
+                console.log(data.data[i] + "とマッチしました")
+                message.reply("不適切な言葉です\n消しました")
+                    .then(() => message.delete());
+                return
+            }
+            else if (message.content === "なんで") {
+                message.reply("悪口だから")
+                return
+            }
 
-            message.reply("不適切な言葉です\n消しました")
-            .then(() => message.delete());
-            return
-            
         }
-        else if(message.content === "なんで"){
-            message.reply("悪口だから")
-            return
-        }
-        
-    }}
+    }
     for (let i = 0; i != data.urldata.length; i++) {
         if (message.content.match(data.urldata[i])) {
             counta++
             console.log(data.urldata[i] + "とマッチしました")
 
             message.reply("不適切な動画URLを消しました")
-            .then(() => message.delete());
+                .then(() => message.delete());
             return
-            
+
         }
     }
     if (message.content === "こんにちは") {
@@ -99,14 +97,14 @@ client.on('messageCreate', message => {  //切れてるのか横も
         console.log("コマンド名\n" + loga + "\n実行user" + username + "による実行")
 
     }
-    if (message.content === "/GUI"){
-		const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('primary')
-					.setLabel('Primary')
-					.setStyle('PRIMARY'),
-			);
+    if (message.content === "/GUI") {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('primary')
+                    .setLabel('Primary')
+                    .setStyle('PRIMARY'),
+            );
     }
     if (message.content === "Hello") {
         cmdexec++
@@ -131,12 +129,12 @@ client.on('messageCreate', message => {  //切れてるのか横も
         message.channel.send("English/goodmorning")
         console.log("コマンド名\n" + loga + "\n実行user" + username + "による実行")
     }
-    if (message.content === "はじめまして"){
+    if (message.content === "はじめまして") {
         const username = message.author.username
-        message.reply("はじめまして"+username+"さん")
+        message.reply("はじめまして" + username + "さん")
         message.channel.send("English/Nice to meet you!")
     }
-    
+
 
 
 
@@ -146,7 +144,7 @@ client.on('ready', async () => {
 })
 process.on("exit", exitCode => {
     console.log(cmdexec)
-    console.log("botが実行されているときに起きた暴言等は"+counta+"回です")
+    console.log("botが実行されているときに起きた暴言等は" + counta + "回です")
 });
 process.on("SIGINT", () => process.exit(0));
 client.login(process.env.token)
