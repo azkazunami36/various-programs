b9f5f47df3b9efe301c51af4b1d891346909acdf
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-27 11:04:37 +0900

    足りないモジュールを追加、小さなバグ修正

--- modules/ytAudioGet.js
+++ modules/ytAudioGet.js
@@ -3,32 +3,44 @@
  * @param {string} videoId 
  */
 const fs = require("fs")
-module.exports.ytAudioGet = videoId => {
+const ytdl = require("ytdl-core")
+const mbyteString = require("./mbyteString").mbyteString
+const timeString = require("./timeString").timeString
+module.exports.ytAudioGet = async videoId => {
     if (!fs.existsSync("cache/YTDl/" + videoId + ".mp3")) {
-        let starttime
-        if (!fs.existsSync("cache/YouTubeDownloadingAudio")) fs.mkdirSync("cache/YouTubeDownloadingAudio")
-        const audioDownload = ytdl(videoId, { filter: "audioonly", quality: "highest" })
-        audioDownload.once("response", () => starttime = Date.now())
-        audioDownload.on("progress", (chunkLength, downloaded, total) => {
-            const floatDownloaded = downloaded / total
-            const downloadedSeconds = (Date.now() - starttime) / 1000
-            //推定残り時間
-            const timeLeft = downloadedSeconds / floatDownloaded - downloadedSeconds
-            //パーセント
-            const percent = (floatDownloaded * 100).toFixed()
-            //ダウンロード済みサイズ
-            const mbyte = mbyteString(downloaded)
-            //最大ダウンロード量
-            const totalMbyte = mbyteString(total)
-            //推定残り時間
-            const elapsedTime = timeString(downloadedSeconds)
-        })
-        audioDownload.on("error", async err => { console.log("Audio Get Error " + videoId) })
-        audioDownload.pipe(fs.createWriteStream("cache/YouTubeDownloadingAudio/" + videoId + ".mp3"))
-        audioDownload.on("end", () => {
-            if (!fs.existsSync("cache/YTDl")) fs.mkdirSync("cache/YTDl")
-            fs.createReadStream("cache/YouTubeDownloadingAudio/" + videoId + ".mp3")
-                .pipe(fs.createWriteStream("cache/YTDl/" + videoId + ".mp3"))
+        await new Promise(resolve => {
+            let starttime
+            if (!fs.existsSync("cache/YouTubeDownloadingAudio")) fs.mkdirSync("cache/YouTubeDownloadingAudio")
+            const audioDownload = ytdl(videoId, { filter: "audioonly", quality: "highest" })
+            audioDownload.once("response", () => {
+                starttime = Date.now()
+                console.log("音声取得開始: " + videoId)
+            })
+            audioDownload.on("progress", (chunkLength, downloaded, total) => {
+                const floatDownloaded = downloaded / total
+                const downloadedSeconds = (Date.now() - starttime) / 1000
+                //推定残り時間
+                const timeLeft = downloadedSeconds / floatDownloaded - downloadedSeconds
+                //パーセント
+                const percent = (floatDownloaded * 100).toFixed()
+                //ダウンロード済みサイズ
+                const mbyte = mbyteString(downloaded)
+                //最大ダウンロード量
+                const totalMbyte = mbyteString(total)
+                //推定残り時間
+                const elapsedTime = timeString(downloadedSeconds)
+            })
+            audioDownload.on("error", async err => { console.log("Audio Get Error " + videoId) })
+            audioDownload.pipe(fs.createWriteStream("cache/YouTubeDownloadingAudio/" + videoId + ".mp3"))
+            audioDownload.on("end", async () => {
+                if (!fs.existsSync("cache/YTDL")) fs.mkdirSync("cache/YTDL")
+                const Stream = fs.createReadStream("cache/YouTubeDownloadingAudio/" + videoId + ".mp3")
+                Stream.pipe(fs.createWriteStream("cache/YTDL/" + videoId + ".mp3"))
+                Stream.on("end", () => {
+                    console.log("音声取得完了: " + videoId)
+                    resolve()
+                })
+            })
         })
     }
 }
--- modules/ytVideoGet.js
+++ modules/ytVideoGet.js
@@ -3,32 +3,44 @@
  * @param {string} videoId 
  */
 const fs = require("fs")
-cmodule.exports.ytVideoGet = videoId => {
+const ytdl = require("ytdl-core")
+const mbyteString = require("./mbyteString").mbyteString
+const timeString = require("./timeString").timeString
+module.exports.ytVideoGet = async videoId => {
     if (!fs.existsSync("cache/YTDl/" + videoId + ".mp4")) {
-        let starttime
-        if (!fs.existsSync("cache/YouTubeDownloadingVideo")) fs.mkdirSync("cache/YouTubeDownloadingVideo")
-        const videoDownload = ytdl(videoId, { filter: "videoonly", quality: "highest" })
-        videoDownload.once("response", () => starttime = Date.now())
-        videoDownload.on("progress", (chunkLength, downloaded, total) => {
-            const floatDownloaded = downloaded / total
-            const downloadedSeconds = (Date.now() - starttime) / 1000
-            //推定残り時間
-            const timeLeft = downloadedSeconds / floatDownloaded - downloadedSeconds
-            //パーセント
-            const percent = (floatDownloaded * 100).toFixed()
-            //ダウンロード済みサイズ
-            const mbyte = mbyteString(downloaded)
-            //最大ダウンロード量
-            const totalMbyte = mbyteString(total)
-            //推定残り時間
-            const elapsedTime = timeString(downloadedSeconds)
-        })
-        videoDownload.on("error", async err => { console.log("Video Get Error " + videoId) })
-        videoDownload.pipe(fs.createWriteStream("cache/YouTubeDownloadingVideo/" + videoId + ".mp4"))
-        videoDownload.on("end", () => {
-            if (!fs.existsSync("cache/YTDl")) fs.mkdirSync("cache/YTDl")
-            fs.createReadStream("cache/YouTubeDownloadingVideo/" + videoId + ".mp4")
-                .pipe(fs.createWriteStream("cache/YTDl/" + videoId + ".mp4"))
+        await new Promise(resolve => {
+            let starttime
+            if (!fs.existsSync("cache/YouTubeDownloadingVideo")) fs.mkdirSync("cache/YouTubeDownloadingVideo")
+            const videoDownload = ytdl(videoId, { filter: "videoonly", quality: "highest" })
+            videoDownload.once("response", () => {
+                starttime = Date.now()
+                console.log("動画取得開始: " + videoId)
+            })
+            videoDownload.on("progress", (chunkLength, downloaded, total) => {
+                const floatDownloaded = downloaded / total
+                const downloadedSeconds = (Date.now() - starttime) / 1000
+                //推定残り時間
+                const timeLeft = downloadedSeconds / floatDownloaded - downloadedSeconds
+                //パーセント
+                const percent = (floatDownloaded * 100).toFixed()
+                //ダウンロード済みサイズ
+                const mbyte = mbyteString(downloaded)
+                //最大ダウンロード量
+                const totalMbyte = mbyteString(total)
+                //推定残り時間
+                const elapsedTime = timeString(downloadedSeconds)
+            })
+            videoDownload.on("error", async err => { console.log("Video Get Error " + videoId) })
+            videoDownload.pipe(fs.createWriteStream("cache/YouTubeDownloadingVideo/" + videoId + ".mp4"))
+            videoDownload.on("end", async () => {
+                if (!fs.existsSync("cache/YTDL")) fs.mkdirSync("cache/YTDL")
+                const Stream = fs.createReadStream("cache/YouTubeDownloadingVideo/" + videoId + ".mp4")
+                Stream.pipe(fs.createWriteStream("cache/YTDL/" + videoId + ".mp4"))
+                Stream.on("end", () => {
+                    console.log("動画取得完了: " + videoId)
+                    resolve()
+                })
+            })
         })
     }
 }
