255459f19f0e6203a2d50669c5d4b80f42cffe05
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-28 15:55:13 +0900

    既に取得済みの場合Promiseが返ってこないバグを修正

--- modules/ytAuthorInfoGet.js
+++ modules/ytAuthorInfoGet.js
@@ -7,10 +7,11 @@ const ytch = require("yt-channel-info")
 module.exports.ytAuthorInfoGet = async (authorId, ytchRawInfoData) => {
     return await new Promise(async (resolve, reject) => { //情報取得をPromiseで待機させる
         //VideoIDから情報を取得
-        if (!ytchRawInfoData[authorId]) await ytch.getChannelInfo({channelId: authorId}).then(info => {
+        if (!ytchRawInfoData[authorId]) await ytch.getChannelInfo({ channelId: authorId }).then(info => {
             console.log("取得完了" + info.author)
             resolve(info)
         })
             .catch((e) => console.log(e))
+        else resolve(ytchRawInfoData[authorId])
     })
 }
