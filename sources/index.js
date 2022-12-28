38207e578f4dc1dc4f9ea37c9d9f4a6b8194048a
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-28 09:51:11 +0900

    トップページに追加情報を埋め込みするコード追加

--- sources/index.js
+++ sources/index.js
@@ -12,11 +12,39 @@ addEventListener("load", async () => {
                 elem.classList.add("Apps");
                 const AppName = document.createElement("div");
                 AppName.innerHTML = app[i].name;
+                AppName.classList.add("appName")
                 const clickme = document.createElement("a");
                 clickme.id = app[i].id + "button";
                 clickme.href = "sources/" + app[i].compact + "/";
                 clickme.classList.add("clickme");
+                const body = document.createElement("div")
+                const script = document.createElement("script")
+                new Promise(resolve => {
+                    const xhr = new XMLHttpRequest()
+                    xhr.open("get", "http://" + location.hostname + ":" + location.port + "/sources/" + app[i].compact + "/info.html")
+                    xhr.send()
+                    xhr.onreadystatechange = () => {
+                        if (xhr.readyState === 4 && xhr.status === 200) {
+                            body.innerHTML = xhr.responseText
+                            resolve()
+                        }
+                    }
+                })
+                new Promise(resolve => {
+                    const xhr = new XMLHttpRequest()
+                    xhr.open("get", "http://" + location.hostname + ":" + location.port + "/sources/" + app[i].compact + "/info.js")
+                    xhr.send()
+                    xhr.onreadystatechange = () => {
+                        if (xhr.readyState === 4 && xhr.status === 200) {
+                            script.innerHTML = xhr.responseText
+                            resolve()
+                        }
+                    }
+                })
+                body.classList.add("info")
                 elem.appendChild(AppName);
+                elem.appendChild(body)
+                elem.appendChild(script)
                 elem.appendChild(clickme);
                 applist.appendChild(elem);
             };
