ac5607f8b101d24acd6f2bdb77cdebfbe94f0a2d
AzkSumWave36 <kazunami36.sum@icloud.com>
2022-12-28 15:29:04 +0900

    誤字を修正

--- modules/ytAuthorIconGet.js
+++ modules/ytAuthorIconGet.js
@@ -8,26 +8,26 @@ const sharp = require("sharp")
 const axios = require("axios")
 /**
  * ChannelIDからサムネイルを取得します。
- * @param {string} channelId 
+ * @param {string} authorId 
  * @param {resize} resize
  */
-module.exports.ytAuthorIconGet = async (channelId, resize) => {
+module.exports.ytAuthorIconGet = async (authorId, resize) => {
     //ytdlの各大きさのサムネイルリストを取得
-    let thumbnails = JSON.parse(fs.readFileSync("data.json")).ytchRawInfoData[videoId].thumbnails
-    if (!fs.existsSync("cache/YouTubeAuthorIcon/" + videoId + ".jpg")) { //画像が無かったら
+    let thumbnails = JSON.parse(fs.readFileSync("data.json")).ytchRawInfoData[authorId].authorThumbnails
+    if (!fs.existsSync("cache/YouTubeAuthorIcon/" + authorId + ".jpg")) { //画像が無かったら
         await new Promise(async resolve => {
             //axiosでデータリクエスト、サムネリストの一番最後が高画質なため、それを取得する
             const imagedata = await axios.get(thumbnails[thumbnails.length - 1].url, { responseType: "arraybuffer" })
-            fs.writeFileSync("cache/YouTubeAuthorIcon/" + videoId + ".jpg", new Buffer.from(imagedata.data), "binary") //保存
-            console.log("高品質サムネイル取得")
+            fs.writeFileSync("cache/YouTubeAuthorIcon/" + authorId + ".jpg", new Buffer.from(imagedata.data), "binary") //保存
+            console.log("高品質アイコン取得")
             resolve()
         })
     }
-    if (resize && !fs.existsSync("cache/YouTubeAuthorIconRatioResize/" + videoId + "-r" + resize.ratio + "-" + resize.size + ".jpg")) {
+    if (resize && !fs.existsSync("cache/YouTubeAuthorIconRatioResize/" + authorId + "-r" + resize.ratio + "-" + resize.size + ".jpg")) {
         await new Promise(async resolve => {
-            const imagedata = fs.readFileSync("cache/YouTubeAuthorIcon/" + videoId + ".jpg", "binary")
+            const imagedata = fs.readFileSync("cache/YouTubeAuthorIcon/" + authorId + ".jpg", "binary")
             //大きさなどを取得
-            const { width, height, type } = await imageSize("cache/YouTubeAuthorIcon/" + videoId + ".jpg")
+            const { width, height, type } = await imageSize("cache/YouTubeAuthorIcon/" + authorId + ".jpg")
             let tmp1 = width //計算のために
             let tmp2 = height
             for (tmp1; tmp2 != 0;) {
@@ -43,10 +43,10 @@ module.exports.ytAuthorIconGet = async (channelId, resize) => {
             const target = (size > height) ? height : size
             for (y; (aspy * y) < target; y++) { } //受け取った指定画質にリサイズするために
             //キャッシュから画像を取得する
-            const Stream = fs.createWriteStream("cache/YouTubeAuthorIconRatioResize/" + videoId + "-r" + resize.ratio + "-" + resize.size + ".jpg")
-            sharp("cache/YouTubeAuthorIcon/" + videoId + ".jpg").resize(aspx * y, aspy * y).pipe(Stream)
+            const Stream = fs.createWriteStream("cache/YouTubeAuthorIconRatioResize/" + authorId + "-r" + resize.ratio + "-" + resize.size + ".jpg")
+            sharp("cache/YouTubeAuthorIcon/" + authorId + ".jpg").resize(aspx * y, aspy * y).pipe(Stream)
             Stream.on("finish", () => {
-                console.log("最適化サムネイル作成")
+                console.log("最適化アイコン作成")
                 resolve()
             })
         })
