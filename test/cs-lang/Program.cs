5b4c8d3f83fe8f6e8bcc2124555cab25d31d6440
AzkSumWave36 <kazunami36.sum@icloud.com>
2023-01-05 04:48:03 +0900

    適当なコード追加

--- test/cs-lang/Program.cs
+++ test/cs-lang/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Windows.Forms;
+
+// See https://aka.ms/new-console-template for more information
+namespace csharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
