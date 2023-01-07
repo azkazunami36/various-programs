d49e0090584812e7c025a10a24d0e324da832405
AzkSumWave36 <kazunami36.sum@icloud.com>
2023-01-07 20:45:26 +0900

    色々修正

--- test/ytコマンドで再生できるdiscordbot.js
+++ test/ytコマンドで再生できるdiscordbot.js
@@ -27,10 +27,12 @@ const client = new Client({
         GatewayIntentBits.MessageContent
     ]
 });
-
+client.on(Events.ClientReady, () => {
+    console.log("応答準備完了")
+})
 client.on(Events.MessageCreate, async message => {
     if (message.content.startsWith("!yt")) {
-        if (message.member.voice.channel) return message.reply("先にボイスチャットに参加してください！")
+        if (!message.member.voice.channel) return message.reply("先にボイスチャットに参加してください！")
         const channel = message.member.voice.channel
 
         let videoId = message.content.split(" ")[1]
@@ -38,8 +40,13 @@ client.on(Events.MessageCreate, async message => {
         const ytdlID = ytdl.validateID(videoId)
         if (ytdlURL) videoId = ytdl.getVideoID(videoId)
         if (!ytdlURL && !ytdlID) {
-            console.log("VideoID検索で次の文字列は該当しませんでした。: " + videoId)
-            const data = await yts({ query: videoId })
+            let search = ""
+            for (let i = 1; i != message.content.split(" ").length; i++){
+                if (search != "") search += " "
+                search += message.content.split(" ")[i]
+            }
+            console.log("VideoID検索で次の文字列は該当しませんでした。: " + search)
+            const data = await yts({ query: search })
             if (data.videos[0]) videoId = data.videos[0].videoId
             else return message.reply("動画が見つかりませんでした！")
         }
@@ -55,16 +62,20 @@ client.on(Events.MessageCreate, async message => {
         })
         const player = createAudioPlayer()
         connection.subscribe(player)
-        const stream = ytdl(plist[channeldata.playing].url, {
+        const stream = ytdl(videoId, {
             filter: "audioonly",
-            quality: "highest", highWaterMark: 1 * 1e6
+            quality: "highest",
+            highWaterMark: 1 * 1e6
         });
         const resource = createAudioResource(stream, { inputType: StreamType.WebmOpus, inlineVolume: true })
         resource.volume.setVolume(0.5)
         player.play(resource)
         await entersState(player, AudioPlayerStatus.Playing);
         await entersState(player, AudioPlayerStatus.Idle);
+        player.on("stateChange", (oldState, newState) => {
+            console.log(oldState, newState)
+        })
         connection.destroy()
     }
 })
-client.login(process.env.token)
+client.login("MTAyODI4NTcyMTk1NTU1MzM2Mg.GoOgO_.S5aVYsytDom75ET1_J9PbjtE19NEzKLpHbObkU")
