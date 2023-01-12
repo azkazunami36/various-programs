fb64e24c3598fbde38a9f522dfdf766802c09997
azkazunami36 <kazunami36.sum@icloud.com>
2023-01-12 22:30:36 +0900

    ストリーム機能を一部改善(macOSに向けて修正を試みるも失敗)

--- modules/VASourceGet.js
+++ modules/VASourceGet.js
@@ -8,19 +8,26 @@ const fs = require("fs")
  */
 module.exports.VASourceGet = async (videopath, range, type, res) => {
     const videoSize = fs.statSync(videopath).size //ファイルサイズ(byte)
-    const chunkSize = videoSize //チャンクサイズ
+    const chunkSize = 1 * 1e6 //チャンクサイズ
 
+    const ranges = String(range).split("-")
     //これは取得するデータ範囲を決定します。
-    const start = Number(((Number(range) < 0 ? 0 : range) || "0").replace(/\D/g, "")) //始まりの指定
-    const end = Math.min(start + chunkSize, videoSize - 1) //終わりの指定
+    const options = { start: 0, end: 0 }
+    options.start = Number(ranges[0].replace(/\D/g, ""))//始まりの指定
+    options.end = Number(ranges[1]) || Math.min(options.start + chunkSize, videoSize - 1) //終わりの指定
 
     const headers = { //ヘッダー
-        "Content-Range": "bytes " + start + "-" + end + "/" + videoSize,
+        "Content-Range": "bytes " + options.start + "-" + options.end + "/" + videoSize,
         "Accept-Ranges": "bytes",
-        "Content-Length": end - start + 1,
+        "Content-Length": String(options.end - options.start + 1),
         "Content-Type": type
     };
-    res.writeHead(206, headers) //206を使用すると接続を続行することが出来る
-    const Stream = fs.createReadStream(videopath, { start: start, end: (end < 0 ? 0 : end) }) //ストリームにし、範囲のデータを読み込む
+    res.writeHead((range) ? 206 : 200, headers) //206を使用すると接続を続行することが出来る
+    const Stream = fs.createReadStream(videopath, options) //ストリームにし、範囲のデータを読み込む
+    Stream.on("error", error => {
+        console.log("Error reading file" + filePath + ".");
+        console.log(error);
+        res.sendStatus(500);
+    });
     Stream.pipe(res) //送信
 }
