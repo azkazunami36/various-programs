1c5455f0de71d4a9577dc273514887e496da6819
azkazunami36 <kazunami36.sum@icloud.com>
2023-01-13 15:59:53 +0900

    誤字の修正とPromiseの追加

--- modules/ytVAMargeConvert.js
+++ modules/ytVAMargeConvert.js
@@ -6,30 +6,32 @@ const fs = require("fs")
  * @param {number} type 0でmp4。1でwebmを選択します。
  */
 module.exports.ytVAMargeConvert = async (videoId, type) => {
-    const savePass = require("../dataPass.json").default
-    if (fs.existsSync(savePass + "cache/YTDL/" + videoId + ".mp4"))
-        await require("./ytVideoGet").ytVideoGet(videoId)
-    if (fs.existsSync(savePass + "cache/YTDL/" + videoId + ".opus"))
-        await require("./ytAudioGet").ytAudioGet(videoId)
-    if (!fs.existsSync(savePass + "cache/YTDL/" + videoId + ".mp4") || !fs.existsSync(savePass + "cache/YTDL/" + videoId + ".opus")) return
-    if (!fs.existsSync(savePass + "cache/YTDLConverting")) fs.mkdirSync(savePass + "cache/YTDLConverting")
-    let ext = ".mp4"
-    const convert = ffmpeg()
-    convert.addInput(savePass + "cache/YTDL/" + videoId + ".mp4")
-    convert.addInput(savePass + "cache/YTDL/" + videoId + ".opus")
-    switch (type) {
-        case 0: convert.addOptions(["-c:v libx264", "-c:a aac", "-tag:v avc1"]); break
-        case 1: convert.addOptions(["-c:v libpx-vp9", "-c:a libopus"]); ext = ".webm"; break
-    }
-    convert.addOptions(["-map 0:v:0", "-map 1:a:0"])
-    convert.save(savePass + "cache/YTDLConverting/" + videoId + ext)
-    convert.on("end", () => {
-        if (!fs.existsSync(savePass + "cache/YTDLConvert")) fs.mkdirSync(savePass + "cache/YTDLConvert")
-        const Stream = fs.createReadStream(savePass + "cache/YTDLConverting/" + videoId + ext)
-        Stream.pipe(fs.createWriteStream(savePass + "cache/YTDLConvert/" + videoId + ext))
-        Stream.on("end", () => {
-            console.log("動画合成完了: " + videoId)
-            resolve()
+    await new Promise(async resolve => {
+        const savePass = require("../dataPass.json").default
+        if (!fs.existsSync(savePass + "cache/YTDL/" + videoId + ".mp4"))
+            await require("./ytVideoGet").ytVideoGet(videoId)
+        if (!fs.existsSync(savePass + "cache/YTDL/" + videoId + ".opus"))
+            await require("./ytAudioGet").ytAudioGet(videoId)
+        if (!fs.existsSync(savePass + "cache/YTDL/" + videoId + ".mp4") || !fs.existsSync(savePass + "cache/YTDL/" + videoId + ".opus")) return
+        if (!fs.existsSync(savePass + "cache/YTDLConverting")) fs.mkdirSync(savePass + "cache/YTDLConverting")
+        let ext = ".mp4"
+        const convert = ffmpeg()
+        convert.addInput(savePass + "cache/YTDL/" + videoId + ".mp4")
+        convert.addInput(savePass + "cache/YTDL/" + videoId + ".opus")
+        switch (type) {
+            case 0: convert.addOptions(["-c:v libx264", "-c:a aac", "-tag:v avc1"]); break
+            case 1: convert.addOptions(["-c:v libvpx-vp9", "-c:a libopus"]); ext = ".webm"; break
+        }
+        convert.addOptions(["-map 0:v:0", "-map 1:a:0"])
+        convert.save(savePass + "cache/YTDLConverting/" + videoId + ext)
+        convert.on("end", () => {
+            if (!fs.existsSync(savePass + "cache/YTDLConvert")) fs.mkdirSync(savePass + "cache/YTDLConvert")
+            const Stream = fs.createReadStream(savePass + "cache/YTDLConverting/" + videoId + ext)
+            Stream.pipe(fs.createWriteStream(savePass + "cache/YTDLConvert/" + videoId + ext))
+            Stream.on("end", () => {
+                console.log("動画合成完了: " + videoId)
+                resolve()
+            })
         })
     })
 }
