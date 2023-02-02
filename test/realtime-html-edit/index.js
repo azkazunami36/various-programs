558f47b2cd3cc47f7cf058a75ed75aed5bdb7c11
azkazunami36 <kazunami36.sum@icloud.com>
2023-02-03 02:16:16 +0900

    さまざまなテストコードの作成と編集。僕はもう覚えてない知らん

--- test/realtime-html-edit/index.js
+++ test/realtime-html-edit/index.js
@@ -3,7 +3,13 @@ addEventListener("load", () => {
     const text = document.getElementById("inputText")
     const base64 = document.getElementById("inputBase64")
     text.addEventListener("input", () => {
-        output.innerHTML = btoa(text.value)
+        let raw = atob(text.value);
+        let HEX = ''
+        for (i = 0; i < raw.length; i++) {
+            let _hex = raw.charCodeAt(i).toString(16)
+            HEX += (_hex.length == 2 ? _hex : '0' + _hex)
+        }
+        output.innerHTML = HEX.toUpperCase().toString(10)
     })
     base64.addEventListener("input", () => {
         output.innerHTML = atob(base64.value)
