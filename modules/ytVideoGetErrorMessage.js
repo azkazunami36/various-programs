963598c3cc6ab70abfef5618ab2dcde7c92261fb
azkazunami36 <kazunami36.sum@icloud.com>
2023-02-07 21:38:02 +0900

    ifの一致方法を修正し、パターンの追加

--- modules/ytVideoGetErrorMessage.js
+++ modules/ytVideoGetErrorMessage.js
@@ -5,18 +5,20 @@
  * @returns 日本語の説明を返すか、nullを返します
  */
 module.exports.ytVideoGetErrorMessage = message => {
-    if (message == "Join this channel to get access to members-only content like this video, and other exclusive perks.")
+    if (message === "Join this channel to get access to members-only content like this video, and other exclusive perks.")
         return "チャンネルのメンバーでないため、データを取得できません。"
-    if (message == "Video unavailable")
+    if (message === "Video unavailable")
         return "このVideoIDは無効なため、データを取得できません。"
-    if (message == "This is a private video. Please sign in to verify that you may see it.")
+    if (message === "This is a private video. Please sign in to verify that you may see it.")
         return "このVideoIDは投稿者が非公開にしているため、データを取得できません。"
-    if (message == "socket hang up")
+    if (message === "socket hang up")
         return "通信エラーによりサーバーが送信を切断しました。"
-    if (message == "aborted")
+    if (message === "aborted")
         return "大きな負荷や遅延等により、通信が切断されました。"
-    if (message == "No such format found: highest")
+    if (message === "No such format found: highest")
         return "要求した品質が存在しないようです。\nこのエラーが複数発生していた場合、VideoIDとともにお知らせください。"
+    if (message === "This video has been removed for violating YouTube's policy on nudity or sexual content")
+        return "ボリシーに違反したため、この動画は削除されています。理由: ヌードや性的コンテンツ"
     if (message.match(/getaddrinfo ENOTFOUND/))
         return "通信エラーにより通信が切断されました。"
     if (message.match(/read ECONNRESET/))
