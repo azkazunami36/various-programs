506773f8c3f33d39e0a2dc9559037df3534497cc
azkazunami36 <kazunami36.sum@icloud.com>
2023-03-05 20:00:01 +0900

    expressクラスの機能追加途中

--- main.ts
+++ main.ts
@@ -1157,6 +1157,7 @@ namespace sumtool {
         }
         /**
          * メモリを使用した後、必ずnullや{}に戻し解放するためのものです。
+         * ,,,もしかしたら今後廃止します。作業効率も悪いですし、何と入っても見づらいです。
          */
         let temp: {
             imageSize?: number,
@@ -1630,10 +1631,42 @@ namespace sumtool {
             console.log(choiceProgramName + "が終了しました。")
         }
     }
+    /**
+     * expressdは名前の通りexpressを利用しますが、少し変わった点があります。  
+     * それは、cuiIOの上位互換だと言うことです。
+     * 
+     * つまり、cuiIOでのプログラムにアクセスする手段のGUI版と言うことです。
+     * そのため、今後cuiIOとguiIOのプログラム内容の同期を行うための、新しい方法を試行錯誤しておきます。
+     * が、それまではプログラムは全く別になり、同じ操作方法を保証することはないです。
+     */
     export class expressd {
         constructor() { }
+        /**
+         * 未完成のため、ここの関数で実行されます。
+         * もしここの関数のみで完成した場合、classを削除しfunctionに置き換えます。
+         * ※sourcedフォルダにデータを入れていますが、main.jsのプログラムを全てここに入れ終えたら、sourcesに名前を変更します。
+         */
         static async main(): Promise<void> {
-
+            const app = express()
+            app.get("", async (req, res) => {
+                /**
+                 * リクエスト(要求)された場所
+                 */
+                const url = req.url
+                /**
+                 * 応答の種類です。
+                 * sourcesフォルダにアクセスするか、その他のデータにアクセスするかを確認し、それに合ったプログラムを実行します。
+                 * pass内に「/」で分割し、それを利用してファイル先へアクセスする手がかりにします。
+                 * 戻り値にファイルパスを入れる必要があります。
+                 */
+                const type: { [type: string]: (pass: string[]) => Promise<string> } = {
+                    "sources": async pass => {
+                        return ""
+                    }
+                }
+                const passArray = url.split("/")
+                const passdata = await type[passArray[1]]([...passArray])
+            })
         }
     }
 }
