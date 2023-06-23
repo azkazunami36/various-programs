251c5a2dcb48a1853ff00360ce5e44efe47b6760
azkazunami36 <kazunami36.sum@icloud.com>
2023-06-24 00:25:24 +0900

    音楽botプログラムの作成

---
+++ test/discord-music-bot/main.ts
@@ -0,0 +1,42 @@
+import Discord from "discord.js"
+import VoDiscord from "@discordjs/voice"
+
+const { Client, GatewayIntentBits, Partials, Events, EmbedBuilder } = Discord
+const { entersState, createAudioPlayer, createAudioResource, joinVoiceChannel, StreamType, AudioPlayerStatus } = VoDiscord
+import ytdl from "ytdl-core"
+import fs from "fs"
+import readline from "readline"
+
+require("dotenv").config()
+
+const client = new Client({
+    partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.GuildScheduledEvent,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.ThreadMember,
+        Partials.User
+    ],
+    intents: [
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildScheduledEvents,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
