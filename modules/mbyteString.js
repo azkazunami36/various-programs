6c5e2d43556298fb01f8f4f9d18a015520ff7653
azkazunami36 <kazunami36.sum@icloud.com>
2023-08-10 04:43:52 +0900

    必要のないと思われるプログラムをすべて削除

--- modules/mbyteString.js
+++
@@ -1,6 +0,0 @@
-/**
- * 数値をByteからMBに変換します。
- * @param {Number} byte Byteを入力します。
- * @returns {Number} MBを返します。
- */
-module.exports.mbyteString = byte => (byte / 1024 / 1024).toFixed(1) 
--- sources/vd/index.js
+++
@@ -1,5 +0,0 @@
-const wait = time => new Promise(resolve => setTimeout(n => resolve(), time))
-const random = num => Math.floor(Math.random() * num)
-addEventListener("load", async e => {
-    console.log(random(100))
-})
