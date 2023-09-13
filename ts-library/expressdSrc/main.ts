ec5511ce2797b4c98db5757a0eedd6aef04540e0
azkazunami36 <kazunami36.sum@icloud.com>
2023-09-13 19:35:16 +0900

    ファイル送信ウィンドウの作成

--- ts-library/expressdSrc/main.ts
+++ ts-library/expressdSrc/main.ts
@@ -1,5 +1,5 @@
 import { windowSystem } from "./js/windowSystem.js"
-import { httpDataRequest, APISend } from "./js/handyTool.js"
+import { httpDataRequest, API } from "./js/handyTool.js"
 
 addEventListener("load", async () => {
     /**
@@ -40,8 +40,60 @@ addEventListener("load", async () => {
     testPost.style.borderRadius = "10px"
     testPost.innerText = "テストPOSTを送信: " + postUrl
     testPost.addEventListener("click", async () => {
-        await APISend("dataIO", JSON.stringify({}))
+    })
+    const fileSendWindow = document.createElement("div")
+    fileSendWindow.style.padding = "5px"
+    fileSendWindow.style.width = "fit-content"
+    fileSendWindow.style.background = "white"
+    fileSendWindow.style.borderRadius = "10px"
+    fileSendWindow.innerText = "ファイル送信"
+    fileSendWindow.addEventListener("click", async () => {
+        const sendWindow = document.createElement("div")
+        sendWindow.style.display = "flex"
+        sendWindow.style.alignItems = "center"
+        sendWindow.style.justifyContent = "center"
+        sendWindow.style.flexDirection = "column"
+        sendWindow.style.background = "rgb(200, 230, 255)"
+        shareData.windowSystem?.createWindow(String(Date.now()), sendWindow, {
+            size: {
+                top: 400,
+                left: 600
+            },
+            title: "ファイル送信",
+            minSize: {
+                top: 150,
+                left: 200
+            }
+        })
+        const fileSelect = document.createElement("input")
+        fileSelect.type = "file"
+        const imgPreview = document.createElement("img")
+        imgPreview.style.width = "100%"
+        imgPreview.style.height = "50%"
+        imgPreview.style.objectFit = "contain"
+        const sendButton = document.createElement("button")
+        sendButton.innerText = "送信"
+        fileSelect.addEventListener("change", async () => {
+            if (fileSelect.files && fileSelect.files[0]) {
+                const data = await fileSelect.files[0].arrayBuffer()
+                console.log(fileSelect.files[0].type)
+                const imgBlob = new Blob([data], { type: fileSelect.files[0].type })
+                imgPreview.src = URL.createObjectURL(imgBlob)
+            }
+        })
+        sendButton.addEventListener("click", async  () => {
+            if (fileSelect.files && fileSelect.files[0]) {
+                const data = await fileSelect.files[0].arrayBuffer()
+                console.log(fileSelect.files[0].type)
+                const imgBlob = new Blob([data], { type: fileSelect.files[0].type })
+                console.log(await API.dataIO.sendFile(imgBlob))
+            }
+        })
+        sendWindow.appendChild(imgPreview)
+        sendWindow.appendChild(fileSelect)
+        sendWindow.appendChild(sendButton)
     })
     mainWindow.appendChild(testPost)
     mainWindow.appendChild(createButton)
+    mainWindow.appendChild(fileSendWindow)
 })
