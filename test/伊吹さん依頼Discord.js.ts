f70e0351157b831558c89052b1796213ce556a2a
azkazunami36 <kazunami36.sum@icloud.com>
2023-10-10 21:48:12 +0900

    依頼されたコードを作成(テストフォルダ

---
+++ test/伊吹さん依頼Discord.js.ts
@@ -0,0 +1,25 @@
+import { Client, GatewayIntentBits, Partials, Events } from "discord.js"
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMessages
+    ]
+})
+
+client.on(Events.MessageCreate, async message => {
+    if (message.author.bot) return
+    /** ここにDMで送り返してほしい文字列を入力する */
+    const sendMessages = [
+        "テスト"
+    ]
+    for (let i = 0; i !== sendMessages.length; i++) if (message.content === sendMessages[i]) {
+        const user = await client.users.fetch("1055399666587545620")
+        if (user) user.send(message.content)
+        break
+    }
+})
+
+client.login("")
